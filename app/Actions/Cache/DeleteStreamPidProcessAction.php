ddd081d7427e9d9f55a4c237b42fe057481d1753
martinfaifer <51059458+martinfaifer@users.noreply.github.com>
2023-05-15 07:30:58 +0200

    fix bug

--- app/Actions/Cache/DeleteStreamPidProcessAction.php
+++ app/Actions/Cache/DeleteStreamPidProcessAction.php
@@ -2,12 +2,14 @@
 
 namespace App\Actions\Cache;
 
+use App\Models\StreamProcessPid;
 use Illuminate\Support\Facades\Cache;
 
 class DeleteStreamPidProcessAction
 {
     public function execute(object $stream)
     {
-        Cache::pull('streamIsMonitoring_'.$stream->id);
+        Cache::pull('streamIsMonitoring_' . $stream->id);
+        StreamProcessPid::where('stream_id', $stream->id)->delete();
     }
 }

b0f2954149f71ef95ad5a495da7dbdddf9e7a042
martinfaifer <51059458+martinfaifer@users.noreply.github.com>
2023-05-18 14:02:18 +0200

    Update GetStreamInformationFromIptvDokuAction.php

--- app/Actions/Streams/API/GetStreamInformationFromIptvDokuAction.php
+++ app/Actions/Streams/API/GetStreamInformationFromIptvDokuAction.php
@@ -13,6 +13,9 @@ class GetStreamInformationFromIptvDokuAction
         //     return Http::withToken($this->token)->get(config('app.iptv_dokumentace')."/api/stream/iptvdohled/{$stream->id}")->json();
         // }
         // return $stream;
+        if (is_null(config('app.iptv_dokumentace_username')) || is_null(config('app.iptv_dokumentace_password'))) {
+            return [];
+        }
         $url = explode(":",  $stream->stream_url);
         return Http::withBasicAuth(config('app.iptv_dokumentace_username'), config('app.iptv_dokumentace_password'))
             ->get(config('app.iptv_dokumentace') . "/api/v2/channels/" .  $url[0])->json();

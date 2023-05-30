36c359cba807d918e3367b837b0d768aef5b5aeb
martinfaifer <51059458+martinfaifer@users.noreply.github.com>
2023-05-30 14:36:17 +0200

    preparing for api

--- app/Http/Controllers/API/ApiStreamController.php
+++ app/Http/Controllers/API/ApiStreamController.php
@@ -2,16 +2,17 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Actions\Streams\DeleteStreamAction;
-use App\Actions\Streams\GetStreamVideoImageAction;
-use App\Actions\Streams\StoreStreamAction;
+use App\Models\Stream;
 use App\Http\Controllers\Controller;
+use App\Actions\Streams\StoreStreamAction;
+use App\Actions\Streams\DeleteStreamAction;
+use App\Http\Resources\ShowAudioPidResource;
+use App\Http\Resources\ShowVideoPidResource;
+use App\Http\Resources\ShowServicePidResource;
 use App\Http\Requests\Api\StreamShowApiRequest;
 use App\Http\Requests\Api\StreamStoreApiRequest;
-use App\Http\Resources\ShowAudioPidResource;
-use App\Http\Resources\ShowServicePidResource;
-use App\Http\Resources\ShowVideoPidResource;
-use App\Models\Stream;
+use App\Actions\Streams\GetStreamVideoImageAction;
+use App\Actions\Streams\ProblemPids\ShowProblemPidsAction;
 
 class ApiStreamController extends Controller
 {
@@ -21,13 +22,14 @@ class ApiStreamController extends Controller
 
         return [
             'status' => 'success',
-            'streamData' => [
+            'data' => [
                 'img' => config('app.url') . (new GetStreamVideoImageAction())->execute($stream, true),
                 'name' => $stream->nazev,
                 'streamStatus' => $stream->status,
                 'streamId' => $stream->id,
                 'monitored_at' => $stream->monitored_at,
                 'history' => [],
+                'problems' => (new ShowProblemPidsAction())->execute($stream),
                 'streamTS' => new ShowServicePidResource($stream),
                 'audioPids' => new ShowAudioPidResource($stream),
                 'videpPids' => new ShowVideoPidResource($stream),

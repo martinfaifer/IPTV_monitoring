794d796a452d3d6abc1c2bc096ae79cb30fbea99
martinfaifer <51059458+martinfaifer@users.noreply.github.com>
2023-06-09 09:02:54 +0200

    disabled wss

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,11 +10,11 @@ window.Pusher = require("pusher-js");
 
 window.Echo = new Echo({
     broadcaster: "pusher",
-    key: "564879849846489",
-    // key: process.env.MIX_PUSHER_APP_KEY,
+    // key: "564879849846489",
+    key: process.env.MIX_PUSHER_APP_KEY,
     wsHost: window.location.hostname,
     wsPort: 6001,
-    wssPort: 6001,
-    // forceTLS: true,
+    // wssPort: 6001,
+    forceTLS: false,
     disableStats: true,
 });

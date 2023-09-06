c8126ebb43fd15b854e510d1f84f0c444a145a74
martinfaifer <51059458+martinfaifer@users.noreply.github.com>
2023-09-06 10:10:00 +0200

    Update FFMpegGetPtsTimeStreamAction.php

--- app/Actions/Streams/FFMpeg/FFMpegGetPtsTimeStreamAction.php
+++ app/Actions/Streams/FFMpeg/FFMpegGetPtsTimeStreamAction.php
@@ -26,7 +26,6 @@ class FFMpegGetPtsTimeStreamAction
         });
 sleep(5);
         $streamPtsFile = file_get_contents(public_path("storage/streamsPts/" . Str::slug($stream->nazev) . ".txt"));
-        dd($streamPtsFile);
         dd(explode("pts_time", $streamPtsFile));
 
         // $ffprobeAnalyzeAction = new FfProbeAnalyzeAction();

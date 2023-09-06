e0f2074e7c3d5775e0291b2a1f34cbe066808032
martinfaifer <51059458+martinfaifer@users.noreply.github.com>
2023-09-06 20:30:34 +0200

    removing checking i frames for iframe modul

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,11 +28,6 @@ class Kernel extends ConsoleKernel
 
         $schedule->command('system:prune')->dailyAt('01:00');
 
-        // $schedule->command('streams:check-pts')->weekly()->mondays()->at('03:00')->runInBackground();
-        // $schedule->command('streams:check-pts')->weekly()->wednesdays()->at('03:00')->runInBackground();
-        // $schedule->command('streams:check-pts')->weekly()->fridays()->at('03:00')->runInBackground();
-        // $schedule->command('streams:check-problemed-pts')->everyFiveMinutes()->runInBackground()->withoutOverlapping();
-
         $schedule->command('streams:start_diagnostic')->everyMinute()->runInBackground();
         $schedule->command('streams:take_statuses_and_store_to_database')->everyMinute()->runInBackground();
         $schedule->command('system:take_network_data')->everyMinute()->runInBackground();

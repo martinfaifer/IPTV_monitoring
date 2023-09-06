4e93e441c1dc8c5ee9334094f8a4af6cd881f72a
martinfaifer <51059458+martinfaifer@users.noreply.github.com>
2023-09-06 14:45:43 +0200

    Update FFMpegGetPtsTimeStreamAction.php

--- app/Actions/Streams/FFMpeg/FFMpegGetPtsTimeStreamAction.php
+++ app/Actions/Streams/FFMpeg/FFMpegGetPtsTimeStreamAction.php
@@ -11,39 +11,45 @@ class FFMpegGetPtsTimeStreamAction
 {
     public function execute(object $stream): mixed
     {
-        $ptsTimes = [];
-        $diffOfPtsTimes = [];
+        try {
+            //code...
 
-        rescue(function () use ($stream) {
-            unlink(public_path("storage/streamsPts/" . Str::slug($stream->nazev) . ".txt"));
-        });
+            $ptsTimes = [];
+            $diffOfPtsTimes = [];
 
-        if (str_contains($stream->stream_url, 'http')) {
-            $command = "ffmpeg -skip_frame nokey -i {$stream->stream_url} -vf showinfo -vsync 0 -f null - > " . public_path("storage/streamsPts/" . Str::slug($stream->nazev) . ".txt") . " 2>&1";
-        } else {
-            $command = "ffmpeg -skip_frame nokey -i udp://{$stream->stream_url} -vf showinfo -vsync 0 -f null - > " . public_path("storage/streamsPts/" . Str::slug($stream->nazev) . ".txt") . " 2>&1";
-        }
+            rescue(function () use ($stream) {
+                unlink(public_path("storage/streamsPts/" . Str::slug($stream->nazev) . ".txt"));
+            });
 
-        rescue(function () use ($command) {
-            Process::timeout(5)->run($command);
-        });
-        sleep(5);
-        $streamPtsFile = file_get_contents(public_path("storage/streamsPts/" . Str::slug($stream->nazev) . ".txt"));
-        $explodedData = (explode("pts_time:", $streamPtsFile));
-
-        foreach ($explodedData as $ptsData) {
-            if (is_numeric(substr($ptsData, 0, 1))) {
-                if (!in_array(substr($ptsData, 0, 1), $ptsTimes)) {
-                    array_push($ptsTimes, substr($ptsData, 0, 1));
-                };
+            if (str_contains($stream->stream_url, 'http')) {
+                $command = "ffmpeg -skip_frame nokey -i {$stream->stream_url} -vf showinfo -vsync 0 -f null - > " . public_path("storage/streamsPts/" . Str::slug($stream->nazev) . ".txt") . " 2>&1";
+            } else {
+                $command = "ffmpeg -skip_frame nokey -i udp://{$stream->stream_url} -vf showinfo -vsync 0 -f null - > " . public_path("storage/streamsPts/" . Str::slug($stream->nazev) . ".txt") . " 2>&1";
             }
+
+            rescue(function () use ($command) {
+                Process::timeout(5)->run($command);
+            });
+            sleep(5);
+            $streamPtsFile = file_get_contents(public_path("storage/streamsPts/" . Str::slug($stream->nazev) . ".txt"));
+            $explodedData = (explode("pts_time:", $streamPtsFile));
+
+            foreach ($explodedData as $ptsData) {
+                if (is_numeric(substr($ptsData, 0, 1))) {
+                    if (!in_array(substr($ptsData, 0, 1), $ptsTimes)) {
+                        array_push($ptsTimes, substr($ptsData, 0, 1));
+                    };
+                }
+            }
+
+            // take diff from last two
+            $allKeysCount = count($ptsTimes) - 1;
+            $diffOfPtsTimes = $ptsTimes[$allKeysCount] - $ptsTimes[$allKeysCount - 1];
+
+            echo $stream->nazev . " ----> " . $diffOfPtsTimes . PHP_EOL;
+            return (int) $diffOfPtsTimes;
+        } catch (\Throwable $th) {
+            return (int) 2;
         }
-
-        // take diff from last two
-        $allKeysCount = count($ptsTimes) - 1;
-        $diffOfPtsTimes = $ptsTimes[$allKeysCount] - $ptsTimes[$allKeysCount - 1];
-
-        echo $stream->nazev . " ----> " . $diffOfPtsTimes. PHP_EOL;
-        return (int) $diffOfPtsTimes;
     }
 }

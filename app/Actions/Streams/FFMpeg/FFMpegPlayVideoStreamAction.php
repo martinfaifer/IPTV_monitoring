cb9153e02acf37f3568eba6cb874a5d61744d38d
martinfaifer <51059458+martinfaifer@users.noreply.github.com>
2023-09-30 08:42:49 +0200

    re

--- app/Actions/Streams/FFMpeg/FFMpegPlayVideoStreamAction.php
+++ app/Actions/Streams/FFMpeg/FFMpegPlayVideoStreamAction.php
@@ -11,7 +11,7 @@ class FFMpegPlayVideoStreamAction
         if (str_contains($stream->stream_url, 'http')) {
             $command = "ffmpeg -i {$stream->stream_url} -c:v libx264 -c:a aac -f hls -hls_time 5 -hls_list_size 5 -hls_flags delete_segments " . public_path("storage/videos/{$stream->id}.m3u8") . " > /dev/null 2>&1 & echo $!; ";
         } else {
-            $command = "ffmpeg -i  udp://{$stream->stream_url} -c:v libx264 -c:a aac -f hls -hls_time 5 -hls_list_size 5 -hls_flags delete_segments " . public_path("storage/videos/{$stream->id}.m3u8") . " > /dev/null 2>&1 & echo $!; ";
+            $command = "ffmpeg -i  udp://{$stream->stream_url} -c:v libx264 -c:a aac -f hls -hls_time 5 -hls_list_size 5 -hls_flags delete_segments " . public_path("storage/videos/{$stream->id}.m3u8") ;
         }
 
         $pid = shell_exec($command);

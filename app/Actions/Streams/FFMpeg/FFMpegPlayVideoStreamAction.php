2d9fb6bc85cffc725fec8931bfd6c98ff9ba171e
martinfaifer <51059458+martinfaifer@users.noreply.github.com>
2023-10-01 10:42:18 +0200

    Update FFMpegPlayVideoStreamAction.php

--- app/Actions/Streams/FFMpeg/FFMpegPlayVideoStreamAction.php
+++ app/Actions/Streams/FFMpeg/FFMpegPlayVideoStreamAction.php
@@ -9,9 +9,9 @@ class FFMpegPlayVideoStreamAction
     public function execute(object $stream): void
     {
         if (str_contains($stream->stream_url, 'http')) {
-            $command = "ffmpeg -i {$stream->stream_url} -c:v libx264 -c:a aac -f hls -hls_time 5 -hls_list_size 5 -hls_flags delete_segments " . public_path("storage/videos/{$stream->id}.m3u8") . " > /dev/null 2>&1 & echo $!; ";
+            $command = "ffmpeg -i {$stream->stream_url} -c:v libx264 -c:a aac -f hls -hls_time 5 -hls_list_size 5 -hls_flags delete_segments " . public_path("storage/videos/{$stream->id}.m3u8");
         } else {
-            $command = "ffmpeg -i  udp://{$stream->stream_url} -c:v libx264 -c:a aac -f hls -hls_time 5 -hls_list_size 5 -hls_flags delete_segments " . public_path("storage/videos/{$stream->id}.m3u8") . " > /dev/null 2>&1 & echo $!; ";
+            $command = "ffmpeg -i  udp://{$stream->stream_url} -c:v libx264 -c:a aac -f hls -hls_time 5 -hls_list_size 5 -hls_flags delete_segments " . public_path("storage/videos/{$stream->id}.m3u8");
         }
 
         $pid = shell_exec($command);

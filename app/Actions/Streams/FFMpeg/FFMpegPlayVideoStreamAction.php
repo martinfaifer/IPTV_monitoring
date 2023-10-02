0c35b46f56534d828bebae252dbacf01713b2588
martinfaifer <51059458+martinfaifer@users.noreply.github.com>
2023-10-02 11:04:12 +0200

    Update FFMpegPlayVideoStreamAction.php

--- app/Actions/Streams/FFMpeg/FFMpegPlayVideoStreamAction.php
+++ app/Actions/Streams/FFMpeg/FFMpegPlayVideoStreamAction.php
@@ -9,10 +9,12 @@ class FFMpegPlayVideoStreamAction
     public function execute(object $stream): void
     {
         if (str_contains($stream->stream_url, 'http')) {
-            $command = "ffmpeg -i {$stream->stream_url} -c:v h264_nvenc -b:v 2000k -c:a aac -strict experimental -b:a 128k " . public_path("storage/videos/{$stream->id}.m3u8");
+            $command = "ffmpeg -i {$stream->stream_url} -c:v h264_qsv -b:v 2000k -c:a aac -strict experimental -b:a 128k " . public_path("storage/videos/{$stream->id}.m3u8");
             // ffmpeg -i {$stream->stream_url} -c:v h264_nvenc -b:v 2000k -c:a aac -strict experimental -b:a 128k public_path("storage/videos/{$stream->id}.m3u8")
+            // ffmpeg -i input_video.mp4 -c:v h264_qsv -b:v 2000k -c:a aac -strict experimental -b:a 128k output_video.mp4
+
         } else {
-            $command = "ffmpeg -i udp://{$stream->stream_url} -c:v h264_nvenc -b:v 2000k -c:a aac -strict experimental -b:a 128k " . public_path("storage/videos/{$stream->id}.m3u8");
+            $command = "ffmpeg -i udp://{$stream->stream_url} -c:v h264_qsv -b:v 2000k -c:a aac -strict experimental -b:a 128k " . public_path("storage/videos/{$stream->id}.m3u8");
             // ffmpeg -i {$stream->stream_url} -c:v h264_nvenc -b:v 2000k -c:a aac -strict experimental -b:a 128k public_path("storage/videos/{$stream->id}.m3u8")
         }
 

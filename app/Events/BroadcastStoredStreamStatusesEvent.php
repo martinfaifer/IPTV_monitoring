2336df5f4dc43d9191773a5a4203dda7bed61f77
martinfaifer <51059458+martinfaifer@users.noreply.github.com>
2023-10-12 14:10:07 +0200

    change broadcast

--- app/Events/BroadcastStoredStreamStatusesEvent.php
+++ app/Events/BroadcastStoredStreamStatusesEvent.php
@@ -5,11 +5,12 @@ namespace App\Events;
 use App\Actions\Streams\HistoryStatuses\ShowStreamsHistoryStatusesAction;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class BroadcastStoredStreamStatusesEvent implements ShouldBroadcastNow
+class BroadcastStoredStreamStatusesEvent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 

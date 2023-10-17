64049696d420f54230267e9137774382b726d3f3
martinfaifer <51059458+martinfaifer@users.noreply.github.com>
2023-10-17 08:08:52 +0200

    .

--- resources/js/components/Settings/Dashboard/settingsDashboardNetwork.vue
+++ resources/js/components/Settings/Dashboard/settingsDashboardNetwork.vue
@@ -48,7 +48,7 @@ export default {
                     },
                 },
                 xaxis: {
-                    type: "datetime",
+                    // type: "datetime",
                     categories: [],
                 },
                 // tooltip: {

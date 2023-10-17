9df361bae11dd9d416f07a8e4c6b096b8cdfa590
martinfaifer <51059458+martinfaifer@users.noreply.github.com>
2023-10-17 09:15:14 +0200

    create command for getting information from gpu

---
+++ app/Console/Commands/GetGPUInformationCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class GetGPUInformationCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'gpu:information';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Getting information about gpu from system and store them. If GPU is not insert or if failing send notification and show in FE';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $nvidiaSmiOutputInXml = shell_exec('nvidia-smi -q -x');
+
+        $nvidiaSmiOutputInJson = json_encode($nvidiaSmiOutputInXml);
+
+        return $nvidiaSmiOutputInJson;
+    }
+}
